98a3fec18257c0295ada55b3256d6ecf72c97ad7
Tom Alexander <tom@saudeagora.com.vc>
2026-04-13 20:14:20 -0300

    aula06 - verificador de par e ímpar

---
+++ src/main/java/Exercicio02.java
@@ -0,0 +1,12 @@
+public static boolean isOdd(int number) {
+  return number % 2 == 0;
+}
+
+void main() {
+  var input = new Scanner(System.in);
+  System.out.println("Digite um número inteiro: ");
+  var number = input.nextInt();
+  var result = isOdd(number) ? "par" : "ímpar";
+  System.out.println("O número " + number + " é " + result);
+  input.close();
+}
